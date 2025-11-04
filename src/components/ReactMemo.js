8623d210e265330cca5f2d5cea6d084846729fd5
aftab-alam02526 <aftab0447@gmail.com>
2025-11-04 13:27:51 +0530

    Update ReactMemo.js

--- src/components/ReactMemo.js
+++ src/components/ReactMemo.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState, memo } from "react";
 
-// ✅ React.memo prevents re-render if props unchanged
-function ReactMemoExample({ text }) {
-  console.log("Rendering Todo:", text);
-  return <li className="todo-item">{text}</li>;
+const SkillItem = memo(({ skill }) => {
+  return <li>{skill}</li>;
+});
+
+export default function ReactMemoExample() {
+  const [skills, setSkills] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAddSkill = () => {
+    if (inputValue.trim() !== "") {
+      setSkills([...skills, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  return (
+    <div className="reactmemo-section">
+      <h2>React Memo testing</h2>
+
+      <input
+        type="text"
+        placeholder="Enter skill"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={handleAddSkill}>Add Skill</button>
+
+      <ul>
+        {skills.map((skill, index) => (
+          <SkillItem key={index} skill={skill} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default React.memo(ReactMemoExample);
