7d530c858f5943a81df7288fe1d330b2544ef34a
aftab-alam02526 <aftab0447@gmail.com>
2025-11-04 13:33:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,11 @@
-import React, { useState, useEffect, useMemo } from "react";
-import "./styles.css";
-import UseMemoExample from "./UseMemo";
-import ReactMemoExample from "./ReactMemo";
+import React from "react";
+import UseMemoExample from "./components/UseMemo";
+import ReactMemoExample from "./components/ReactMemo";
 
 export default function App() {
-  const [todos, setTodos] = useState(["Learn React"]);
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    console.log("Todos updated:", todos);
-  }, [todos]);
-
-  const todoCount = useMemo(() => todos.length, [todos]);
-
-  const addTodo = () => {
-    setTodos([...todos, "New todo"]);
-  };
-
-  const incrementCounter = () => setCounter(counter + 1);
-
   return (
-    <div id="main" className="app-container">
-      <h1>React Hooks App</h1>
-
-      <div className="counter-section">
-        <h2>Counter: {counter}</h2>
-        <button onClick={incrementCounter}>Increment</button>
-      </div>
-
-  
-      <UseMemoExample onAddTodo={addTodo} todos={todos} count={todoCount} />
-
-     
+    <div id="main">
+      <UseMemoExample />
       <ReactMemoExample />
     </div>
   );
